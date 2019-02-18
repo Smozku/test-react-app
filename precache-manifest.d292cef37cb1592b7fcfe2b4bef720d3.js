9c74fc195c9d160f43c01968bd76656bfaba71ad
Smozku <smozku@gmail.com>
2019-02-18 14:35:31 +0100

    Updates

---
+++ precache-manifest.d292cef37cb1592b7fcfe2b4bef720d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/test-react-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "30d6f604728cd4e4d2a9",
+    "url": "/test-react-app/static/js/runtime~main.30d6f604.js"
+  },
+  {
+    "revision": "7bef22ed7fbf7f0dc5a7",
+    "url": "/test-react-app/static/js/main.7bef22ed.chunk.js"
+  },
+  {
+    "revision": "4c1300154c38b2288d04",
+    "url": "/test-react-app/static/js/1.4c130015.chunk.js"
+  },
+  {
+    "revision": "7bef22ed7fbf7f0dc5a7",
+    "url": "/test-react-app/static/css/main.e97e0f45.chunk.css"
+  },
+  {
+    "revision": "bca4f3607fec6229d05659b66117235c",
+    "url": "/test-react-app/index.html"
+  }
+];
